e55634524de6372aa8dd55f08fb5c61335ab68d6
Aleksey Zolotarev <Alexiy_54@mail.ru>
2024-01-14 17:18:46 +0300

    branch1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Branch3");
+        BranchTest2 branchTest2 = new BranchTest2();
+        branchTest2.printBranchTest2();
+        branchTest2.printBranchTest3();
+        System.out.println("Branch3");
+
     }
 }
