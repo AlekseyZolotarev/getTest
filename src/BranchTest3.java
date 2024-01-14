3a47e2be63131a10ef7e851ace0ba69843365f8d
Aleksey Zolotarev <Alexiy_54@mail.ru>
2024-01-14 17:16:07 +0300

    1 BranchTest3

---
+++ src/BranchTest3.java
@@ -0,0 +1,9 @@
+public class BranchTest3 {
+    public BranchTest3() {
+        System.out.println("BranchTest3 создан");
+    }
+
+    public void printBranchTest3 () {
+        System.out.println("printBranchTest3");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         BranchTest2 branchTest2 = new BranchTest2();
         branchTest2.printBranchTest2();
+        BranchTest3 branchTest3 = new BranchTest3();
+        branchTest3.printBranchTest3();
     }
 }
