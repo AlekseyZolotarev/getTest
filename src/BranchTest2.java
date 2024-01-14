ae13b41c03b0f58fb489e48648854b14b4c6b7b3
Aleksey Zolotarev <Alexiy_54@mail.ru>
2024-01-14 16:46:48 +0300

    2 master

---
+++ src/BranchTest2.java
@@ -0,0 +1,10 @@
+public class BranchTest2 {
+    public BranchTest2() {
+        System.out.println("BranchTest2 создан!");
+    }
+
+    void printBranchTest2 () {
+        System.out.println("printBranchTest2");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        BranchTest2 branchTest2 = new BranchTest2();
+        branchTest2.printBranchTest2();
     }
 }
